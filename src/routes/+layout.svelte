168140dace2e9cfb8b9fb4464f3694845f63363d
Andrew Brown <andrewbrown600@gmail.com>
2024-02-19 10:33:21 -0800

    updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
     import "../app.css";
 </script>
 
-<main class="bg-orange-50 h-[100vh] w-[100vw]">
-    <slot />
+<main class="bg-orange-50 h-[100vh]">
+    <div
+        class="container mx-auto md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-xl"
+    >
+        <slot />
+    </div>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,52 @@
 <div>
-    <div class="flex flex-col space-y-8">
-        <header class="h-16 flex items-center border-b-2 border-orange-100">
-            <div
-                class="font-semibold text-sm container mx-8 sm:mx-16 md:mx-32 lg:mx-64"
-            >
-                ~/moduspwnens
-            </div>
+    <div class="flex flex-col space-y-16">
+        <header class="h-16 border-b-2 flex items-center space-x-16">
+            <div class="font-semibold text-sm">moduspwnens</div>
+            <nav>
+                <ul class="">
+                    <li>
+                        ~/ <a href="/" class=" text-blue-300">Home</a>
+                    </li>
+                    <li>
+                        / <a href="/blog" class="text-blue-300"> Blog</a>
+                    </li>
+                    <li>
+                        / <span> Lorem Ipsum</span>
+                    </li>
+                </ul>
+            </nav>
         </header>
-        <div class="mx-8 sm:mx-16 md:mx-32 lg:mx-64 space-y-8 overflow-x-auto">
-            <ul>
-                <li>
-                    <a href="/blog font-medium">Blog</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-                <li>
-                    <a href="/contact">Contact</a>
-                </li>
-            </ul>
-            <p class="text-wrap">
-                lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+        <div class="space-y-8">
+            <div class="grid grid-cols-8">
+                <aside class="col-span-2">blog posts...</aside>
+                <div class="col-span-4 space-y-8">
+                    <h1 class="text-4xl font-bold">
+                        Lorem Ipsum Dolor Sit Amet
+                    </h1>
+
+                    <p class="text-wrap">
+                        lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+<style>
+    li {
+        display: inline-block;
+    }
+    a {
+        font-weight: 500;
+        color: var(sky-300);
+    }
+</style>
