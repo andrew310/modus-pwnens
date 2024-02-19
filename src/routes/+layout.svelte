e1586c66bd839d69f95f635a0215256e6d338680
Andrew Brown <andrewbrown600@gmail.com>
2024-02-19 09:44:25 -0800

    initialize repo

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    import "../app.css";
+</script>
+
+<main class="bg-orange-50 h-[100vh] w-[100vw]">
+    <slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<div>
+    <div class="flex flex-col space-y-8">
+        <header class="h-16 flex items-center border-b-2 border-orange-100">
+            <div
+                class="font-semibold text-sm container mx-8 sm:mx-16 md:mx-32 lg:mx-64"
+            >
+                ~/moduspwnens
+            </div>
+        </header>
+        <div class="mx-8 sm:mx-16 md:mx-32 lg:mx-64 space-y-8 overflow-x-auto">
+            <ul>
+                <li>
+                    <a href="/blog font-medium">Blog</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+            </ul>
+            <p class="text-wrap">
+                lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+    </div>
+</div>
